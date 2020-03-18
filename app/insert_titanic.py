f9c5e72cc8b4952e739a36ce0376a7e9981004b1
Chris Jakuc <chris.jakuc@gmail.com>
2020-03-17 21:09:24 -0400

    Finished part 2 of assignment

--- app/insert_titanic.py
+++ app/insert_titanic.py
@@ -17,37 +17,61 @@ connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
 
 cursor = connection.cursor()
 
+temp = """
+DROP TABLE survival_status, attributes;
+"""
+cursor.execute(temp)
+
 # 
 # TABLE 1 CREATION
 #
-query = """
+survival_query = """
 CREATE TABLE IF NOT EXISTS survival_status (
-  id SERIAL PRIMARY KEY
-  ,survived int NOT NULL
+  id INTEGER PRIMARY KEY NOT NULL
+  ,Survived INTEGER NOT NULL
 );
 """
-cursor.execute(query)
-cursor.execute("SELECT * from titanic_table;")
-result = cursor.fetchall()
-print("RESULT:", result)
+cursor.execute(survival_query)
+
+# Insert values into survival_status table
+survival_insertion_query = """INSERT INTO survival_status (id, Survived)
+VALUES %s
+ON CONFLICT DO NOTHING
+"""
+survival_df = df['Survived']
+survival_list = list(zip(survival_df.index,survival_df))
+execute_values(cursor, survival_insertion_query, survival_list)
+
+
 
 # 
 # TABLE 2 CREATION
 # 
 attribute_query="""
-CREATE TYPE sex AS ENUM ('male', 'female');
 CREATE TABLE IF NOT EXISTS attributes (
-  id SERIAL PRIMARY KEY
-  ,Pclass int NOT NULL
-  ,Name varchar(40) NOT NULL
-  ,Sex sex
-  ,Age int NOT NULL
-  ,Sibling/Spouses_Aboard int NOT NULL
-  ,Parents/Children_Aboard int NOT NULL
-  ,Fare numeric NOT NULL
+  id INTEGER REFERENCES survival_status (id)
+  ,Pclass INTEGER NOT NULL
+  ,Name TEXT NOT NULL
+  ,Sex TEXT NOT NULL
+  ,Age INTEGER NOT NULL
+  ,Sibling_Spouses_Aboard INTEGER NOT NULL
+  ,Parents_Children_Aboard INTEGER NOT NULL
+  ,Fare REAL NOT NULL
 );
 """
+cursor.execute(attribute_query)
 
-# Insert data into table 1
-insertion_query = "INSERT INTO survival_status (id, data) VALUES %s"
 
+
+# Insert data into attributes table
+attributes_insertion_query = """INSERT INTO attributes (Pclass,Name,Sex,Age,Sibling_Spouses_Aboard,Parents_Children_Aboard,Fare)
+VALUES %s
+ON CONFLICT DO NOTHING
+"""
+attributes_df = df.drop(columns=['Survived'])
+attributes_list = list(attributes_df.itertuples(index=False))
+execute_values(cursor, attributes_insertion_query, attributes_list)
+
+
+# ACTUALLY SAVE THE TRANSACTIONS
+connection.commit()
